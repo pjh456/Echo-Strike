42cb7fa142ad3187c864ae9fba01c688e8527bb7
pjh456 <147148383@qq.com>
2025-10-02 07:14:57 +0800

    fix: 修复了 SDL 重新构建时无法匹配路径的问题

--- tests/test_sdl.cpp
+++ tests/test_sdl.cpp
@@ -3,63 +3,25 @@
 #include <SDL3_ttf/SDL_ttf.h>
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    if (!SDL_Init(SDL_INIT_VIDEO))
+
     {
-        printf("SDL 初始化失败: %s\n", SDL_GetError());
-        return -1;
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << "\n";
+        return 1;
     }
 
-    auto window = SDL_CreateWindow(
-        "SDL3 Demo",
-        800, 600, // 宽高
-        SDL_WINDOW_RESIZABLE);
-
-    if (!window)
+    if (!TTF_Init())
     {
-        printf("窗口创建失败: %s\n", SDL_GetError());
-        SDL_Quit();
-        return -1;
+        std::cerr << "TTF_Init Error: " << SDL_GetError() << "\n";
+        return 1;
     }
 
-    auto renderer = SDL_CreateRenderer(window, NULL);
-    if (!renderer)
-    {
-        printf("渲染器创建失败: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+    std::cout << "SDL3, SDL3_image, SDL3_ttf all initialized successfully!\n";
 
-    int running = 1;
-    while (running)
-    {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_EVENT_QUIT)
-            { // 点击关闭按钮
-                running = 0;
-            }
-        }
-
-        // 设置背景色（R,G,B,A）
-        SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255);
-        SDL_RenderClear(renderer);
-
-        // 画一个矩形
-        SDL_FRect rect = {200, 150, 400, 300};
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
-
-        // 交换缓冲区
-        SDL_RenderPresent(renderer);
-    }
-
-    // 清理资源
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    // 清理
+    TTF_Quit();
     SDL_Quit();
     return 0;
 }
