40744362542902cd9bc971c61e1fa02c82a32170
pjh456 <147148383@qq.com>
2025-10-07 19:57:26 +0800

    feat: 初步搭建 Player 类

--- include/echo_strike/entity/entities/player.hpp
+++ include/echo_strike/entity/entities/player.hpp
@@ -2,9 +2,44 @@
 #define INCLUDE_PLAYER
 
 #include <echo_strike/entity/entity.hpp>
+#include <echo_strike/entity/entity_state.hpp>
+#include <echo_strike/device/input_manager.hpp>
+#include <echo_strike/config/resource_manager.hpp>
+#include <echo_strike/device/device_manager.hpp>
 
 class Player : public Entity
 {
+private:
+    const float SPEED_RUN = 300.0f;
+    const float SPEED_JUMP = 780.0f;
+    const float SPEED_ROLL = 800.0f;
+
+public:
+    Player()
+    {
+        auto &manager = ResourceManager::instance();
+        auto renderer = DeviceManager::instance().get_renderer();
+
+        auto idle_anim = new EntityState();
+        auto [idle_count, idle_atlas] =
+            manager.load_atlas(
+                renderer->get_renderer(),
+                "player/idle");
+        idle_anim->get_anim().add_frames(*idle_atlas);
+        anim_sm.register_state("idle", idle_anim);
+    }
+
+    ~Player()
+    {
+    }
+
+public:
+    void on_update(float delta) override
+    {
+        Entity::on_update(delta);
+        if (m_rect.top() == FLOOR)
+            anim_sm.switch_to("idle");
+    }
 };
 
 #endif // INCLUDE_PLAYER
